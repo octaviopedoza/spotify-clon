48a943d65253ea8d11244f3a629a329a5f6aab29
octaviopedoza <octavio.pedroza.bn@gmail.com>
2023-01-09 18:49:59 -0600

    Se agrega e importa el modulo de FormGroup

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: AuthPageComponent},
+  {path:'**', redirectTo: '/auth/login'} //redireccion a ogin si la ruta no existe
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
   styleUrls: ['./auth-page.component.css']
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit{
+  formLogin: FormGroup = new FormGroup({});
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
