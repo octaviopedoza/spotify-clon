9f21f6882040dfb1964a15a6e9fad4e8c3adf97e
octaviopedoza <octavio.pedroza.bn@gmail.com>
2023-01-27 17:27:27 -0600

    Login terminado

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 
 const routes: Routes = [
-  {path:'login', component: AuthPageComponent},
-  {path:'**', redirectTo: '/auth/login'} //redireccion a ogin si la ruta no existe
+  {path:'login', component: AuthPageComponent}, //ruta para acceder al login
+  {path:'**', redirectTo: '/auth/login'} //redireccion a login si la ruta no existe (sirve para rutas 404)
 ];
 
 @NgModule({
--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup} from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
@@ -11,7 +11,25 @@ export class AuthPageComponent implements OnInit{
   constructor(){}
 
   ngOnInit(): void {
-    
+    this.formLogin = new FormGroup(
+      {
+        email:new FormControl('',[
+          // Validaciones basicas
+          Validators.required, //validamos que exista el campo
+          Validators.email //Validamos que el campo sea de tipo email
+        ]),
+        password: new FormControl('',[
+          Validators.required, //campo requerido
+          Validators.minLength(6), //minimo de caracteres a aceptar
+          Validators.maxLength(12) //maximo de caracteres a aceptar
+        ])
+      }
+    )
   }
-
+  
+// funcion que captura los datos que se envian desde el formulario de login
+  sendLogin(): void{
+    const body = this.formLogin.value
+    console.log("Puto", body);
+  }
 }
