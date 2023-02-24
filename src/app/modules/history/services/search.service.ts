45d32d0b62f5e74ca1954222bebeb757c1a12de3
octaviopedoza <octavio.pedroza.bn@gmail.com>
2023-02-24 17:27:15 -0600

    se agrega pipe async dentro de la busqueda

--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../../env/environment';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +12,8 @@ export class SearchService {
 
   searchTracks$(term: string): Observable<any> {
     return this.http.get(`${this.URL}/tracks?src=${term}`)
+    .pipe(
+      map((dataRaw:any) => dataRaw.data)
+    )
   }
 }
