06212e8339f1b636612bd905676cedc5cf2ece13
octaviopedoza <octavio.pedroza.bn@gmail.com>
2023-02-24 17:04:05 -0600

    Buscador input creado

--- src/app/modules/history/pages/history-page/history-page.component.ts
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { TrackModel } from './../../../../core/models/tracks.model';
+import { SearchService } from './../../services/search.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent {
+export class HistoryPageComponent implements OnInit{
+  listResults:TrackModel[] = []
+
+  constructor(private searchService: SearchService){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  receiveData(event: string): void{
+    console.log("Escucho desde history-page component",event)
+    this.searchService.searchTracks$(event)
+    .subscribe(({data}) => {
+      this.listResults = data;
+    })
+  }
 
 }
--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,6 +1,6 @@
 import { TrackModel } from './../../../core/models/tracks.model';
-import { Component, OnInit} from '@angular/core';
-import * as dataRaw from '../../../data/tracks.json' //importamos todos los datos del dataset dentro de dataRaw
+import { Component, Input, OnInit} from '@angular/core';
+// import * as dataRaw from '../../../data/tracks.json' //importamos todos los datos del dataset dentro de dataRaw
 
 @Component({
   selector: 'app-play-list-body',
@@ -8,15 +8,15 @@ import * as dataRaw from '../../../data/tracks.json' //importamos todos los dato
   styleUrls: ['./play-list-body.component.css']
 })
 export class PlayListBodyComponent implements OnInit{
-  tracks: TrackModel[] = [];
+  @Input() tracks: TrackModel[] = [];
   optionSort:{property:string | null, order:string} = {property:null, order:'asc'}
   constructor() {}
 
   ngOnInit(): void {
-    // Creamos una constante con destructuracion de JS llamada data y la hacemos igual a dataRaw y decimos que queremos sacar el default
-    const {data}:any = (dataRaw as any).default
-    // el Array tracks que acabamos de crear arriba va a ser igual a la constante data
-    this.tracks = data;
+    // // Creamos una constante con destructuracion de JS llamada data y la hacemos igual a dataRaw y decimos que queremos sacar el default
+    // const {data}:any = (dataRaw as any).default
+    // // el Array tracks que acabamos de crear arriba va a ser igual a la constante data
+    // this.tracks = data;
   }
 
   changeSort(property:string):void{
